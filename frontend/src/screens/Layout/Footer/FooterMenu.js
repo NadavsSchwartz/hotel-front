3dd3015ad39d7189a6e3a432ab337e3575b0dbef
nadavschwartz <nadavschwartz58@gmail.com>
2021-12-30 08:00:47 -0800

    correct links on footer and side menu

--- frontend/src/screens/Layout/Footer/FooterMenu.js
+++ frontend/src/screens/Layout/Footer/FooterMenu.js
@@ -6,13 +6,16 @@ const FooterMenu = () => {
 	return (
 		<Menu>
 			<Menu.Item key='0'>
-				<NavLink to='/dashboard'>Dashboard</NavLink>
+				<NavLink to='/'>Home</NavLink>
 			</Menu.Item>
 			<Menu.Item key='1'>
-				<NavLink to='/latest'>Recent Searches</NavLink>
+				<NavLink to='/recent-deals'>Recent Searches</NavLink>
 			</Menu.Item>
 			<Menu.Item key='2'>
 				<NavLink to='/privacy'>Privacy</NavLink>
+			</Menu.Item>
+						<Menu.Item key='2'>
+				<NavLink to='/terms'>Terms</NavLink>
 			</Menu.Item>
 		</Menu>
 	);
--- frontend/src/screens/Layout/Header/MobileMenu.js
+++ frontend/src/screens/Layout/Header/MobileMenu.js
@@ -1,27 +1,18 @@
-import React, { useContext } from 'react';
+import React  from 'react';
 import { NavLink } from 'react-router-dom';
 import { Menu } from 'antd';
-// import { AuthContext } from 'context/AuthProvider';
-// import {
-//   HOME_PAGE,
-//   LISTING_POSTS_PAGE,
-//   PRICING_PLAN_PAGE,
-//   AGENT_ACCOUNT_SETTINGS_PAGE,
-// } from 'settings/constant';
-
+ 
 const MobileMenu = ({ className }) => {
-  // auth context
-  // const { loggedIn, logOut } = useContext(AuthContext);
-
+ 
   return (
     <Menu className={className}>
       <Menu.Item key="0">
-        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/">Home</NavLink>
+      </Menu.Item>
+      <Menu.Item key="0">
+        <NavLink to="/recent-deals">Recent Searches</NavLink>
       </Menu.Item>
       <Menu.Item key="1">
-        <NavLink to="/latest">Recent Searches</NavLink>
-      </Menu.Item>
-      <Menu.Item key="2">
         <NavLink to="/privacy">Privacy</NavLink>
       </Menu.Item>
     </Menu>
