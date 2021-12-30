1900aff42c1da00072961513694292f759edffa1
nadavschwartz <nadavschwartz58@gmail.com>
2021-12-30 12:53:39 -0800

    update reference to build folder

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import hotelDealsRoutes from './routes/hotelDealsRoutes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+
 dotenv.config();
 
 connectDB();
@@ -16,21 +17,21 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+	);
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+	app.get('/', (req, res) => {
+		res.send('API is running....');
+	});
 }
 app.use('/api/v1/', hotelDealsRoutes);
 app.use(notFound);
@@ -39,6 +40,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV}, mode on port ${PORT}`)
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV}, mode on port ${PORT}`)
 );
